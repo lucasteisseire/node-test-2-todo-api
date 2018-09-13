06f5f466427783dfecd1166f2f76001a8653c643
Lucas Teisseire <lucasteisseire@MacBook-Pro-de-Lucas.local>
2018-09-13 16:19:13 +0200

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+const {MongoClient, ObjectID} = require('mongodb')
+
+
+var url = 'mongodb://localhost:27017/TodoApp'
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to database')
+    }
+    console.log('Connected to MongoDB server')
+    const db = client.db('TodoApp')
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id : ObjectID("5b9a5914b332e422131354ca"),
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result)
+    // })
+
+    db.collection('Users').findOneAndUpdate({
+        _id: ObjectID("5b9a56599acfd362fc9c09ae")
+    }, {
+       $set: {
+           name: 'lucas'
+       },
+       $inc: {
+           age: 1 
+       }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(JSON.stringify(result))
+    }
+    )
+    // db.close();
+})
