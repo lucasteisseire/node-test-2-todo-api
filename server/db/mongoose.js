d48b937e5cea676ec19429dfc64dcef7488e4728
Lucas Teisseire <lucasteisseire@MacBook-Pro-de-Lucas.local>
2018-09-15 23:49:27 +0200

    Add POST /users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true)
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose}
